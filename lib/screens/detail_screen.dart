5d9bbdf96aa137c16257b8d5b47afae82566f9d2
natasyaa10 <natasyapriscill10@gmail.com>
2026-03-10 17:27:07 +0700

    Tanggal 10 maret 2026

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_daftar_movie/models/movie.dart';
+import 'package:flutter_daftar_movie/screens/detail_screen.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Movie movie;
+
+  const DetailScreen({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(movie.title)),
+      body: Center(child: Text('Detail untuk: ${movie.title}')),
+    );
+  }
+}
